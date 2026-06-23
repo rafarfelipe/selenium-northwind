05f9d05eb729a8c5527b036e3374f4483f24b6a9
Rafael Felipe <rafarfelipe91@gmail.com>
2026-06-23 16:39:38 -0300

    feat(login): add teste de acesso sem credenciais com assert de mensagem de erro

--- src/test/java/app/vercel/northwind/login/LoginTest.java
+++ src/test/java/app/vercel/northwind/login/LoginTest.java
@@ -1,4 +1,35 @@
 package app.vercel.northwind.login;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    }
+
+    @Test
+    public void testValidarAcessoSemCredeciais() throws Exception {
+        driver.get("https://northwind-test-platform.vercel.app/");
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Assertions.assertTrue(
+                driver.findElement(By.cssSelector("[data-testid='password-error']")).isDisplayed(),"Email e senha são obrigatórios");
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
 }
